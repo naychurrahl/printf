5c770a37df7944056b754af2c8f067eb3efa228b
naychurrahl <onifadej@gmail.com>
2022-10-21 09:51:28 +0100

    Momment of truth

---
+++ numpluscase.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * convert - number and base into string
+ * @num: function number
+ * @base: function base
+ * @lowercase: flag if hexa values need to be lowercase
+ * Return: return string
+ */
+char *convert(unsigned long int num, int base, int lowercase)
+{
+	static char *rep;
+	static char buffer[50];
+	char *ptr;
+
+	rep = (lowercase)
+		? "0123456789abcdef"
+		: "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = rep[num % base];
+		num /= base;
+	} while (num != 0);
+
+	return (ptr);
+}
